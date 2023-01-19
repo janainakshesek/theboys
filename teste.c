05096333868c092a22a722f985b111332efc5dae
Janaína Saldiva Kshesek <65553897+janainakshesek@users.noreply.github.com>
2023-01-19 09:35:29 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "libfila.h"
+#include "liblef.h"
+
+evento_t *cria_evento(){
+    evento_t *e;
+
+    e = malloc(sizeof(evento_t));
+    if(e == NULL)
+        return NULL;
+    e->dado1 = 20;
+    e->dado2 = 10;
+    e->tempo = 3;
+    e->tipo = 4;
+
+    return e;
+}
 
 
 int main(){
-    fila_t *f;
+    lef_t *l;
+    evento_t *e;
+    
+    e = cria_evento();
 
-    f = cria_fila();
-    destroi_fila(f);
+
+    l = cria_lef();
+
+    adiciona_inicio_lef(l, e);
+
+    destroi_lef(l);
+    free(e);
 
     return 0;
 
