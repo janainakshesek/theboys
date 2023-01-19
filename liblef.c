1e4b219de261b245b60c3050aa0c2909dcda64a6
Janaína Saldiva Kshesek <65553897+janainakshesek@users.noreply.github.com>
2023-01-19 17:02:47 -0300

    Update liblef.c

--- liblef.c
+++ liblef.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "liblef.h"
+#include <string.h>
 
 
 /* 
@@ -10,7 +11,7 @@ lef_t * cria_lef (){
     lef_t *l;
 
     l = malloc(sizeof(lef_t));
-    if(l == NULL)
+    if (l == NULL)
         return NULL;
     l->Primeiro = NULL;
     return l;
@@ -22,9 +23,10 @@ lef_t * cria_lef (){
 lef_t *destroi_lef (lef_t *l){
     nodo_lef_t *aux;
 
-    while(l->Primeiro != NULL){
+    while( l->Primeiro != NULL){
         aux = l->Primeiro;
         l->Primeiro = l->Primeiro->prox;
+        free(aux->evento);
         free(aux);
     }
 
@@ -47,8 +49,16 @@ int adiciona_inicio_lef (lef_t *l, evento_t *evento){
     if (n == NULL)
         return 0;
     
-    n->evento = evento;
-    n->prox = NULL;
+    n->evento = malloc(sizeof(evento_t));
+    if ((n->evento) == NULL)
+        return 0;
+
+    memcpy(n->evento, evento, sizeof(evento_t));
+
+    if (l->Primeiro == NULL)
+        n->prox = NULL;
+    
+    n->prox = l->Primeiro;
     l->Primeiro = n;
 
     return 1;
@@ -59,7 +69,36 @@ int adiciona_inicio_lef (lef_t *l, evento_t *evento){
  * Uma nova copia da estrutura evento sera feita 
  * Retorna 1 em caso de sucesso, 0 caso contrario.
  */
-int adiciona_ordem_lef (lef_t *l, evento_t *evento);
+int adiciona_ordem_lef (lef_t *l, evento_t *evento){
+    nodo_lef_t *novo, *aux;
+
+    novo = malloc(sizeof(nodo_lef_t));
+    if (novo == NULL)
+        return 0;
+
+    (novo->evento) = malloc(sizeof(evento_t));
+    if ((novo->evento) == NULL)
+        return 0;
+
+    memcpy(novo->evento, evento, sizeof(evento_t));
+    
+    if (novo->evento->tempo < l->Primeiro->evento->tempo)
+        return adiciona_inicio_lef(l, evento);
+    
+    if (!(novo = malloc(sizeof(nodo_lef_t))))
+        return 0;
+
+    aux = l->Primeiro;
+
+    while (aux->prox != NULL && aux->prox->evento->tempo < novo->evento->tempo){
+        aux = aux->prox;
+    }
+
+    novo->prox = aux->prox;
+    aux->prox = novo;
+    return 1;
+
+}
 
 /* 
  * Retorna e retira o primeiro evento da LEF. 
@@ -67,5 +106,3 @@ int adiciona_ordem_lef (lef_t *l, evento_t *evento);
  * a memoria associada eh de quem chama essa funcao.
  */
 evento_t *obtem_primeiro_lef (lef_t *l);
-
-
